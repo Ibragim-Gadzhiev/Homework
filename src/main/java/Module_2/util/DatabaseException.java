cbd6edc1a4e559de2ddcd1f852a557e898b64fa3
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-04-22 22:17:04 +0300

    Added DatabaseException.java

---
+++ src/main/java/Module_2/util/DatabaseException.java
@@ -0,0 +1,7 @@
+package Module_2.util;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
