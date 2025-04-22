199d7c5fe693e7675df74b83d2c09fa7f647bb6b
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-04-22 22:15:02 +0300

    Added UserDao.java

---
+++ src/main/java/Module_2/dao/UserDao.java
@@ -0,0 +1,15 @@
+package Module_2.dao;
+
+import Module_2.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    void create(User user);
+    Optional<User> read(Long id);
+    List<User> readAll();
+    boolean update(User user);
+    boolean delete(Long id);
+    boolean existsByEmail(String email);
+}
