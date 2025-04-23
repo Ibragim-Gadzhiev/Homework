3db5b84a9efc1c164400689b49cab6827ef945ca
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-04-23 23:51:19 +0300

    docs(style): add Javadoc to TransactionUtil class

--- src/main/java/Module_2/util/TransactionUtil.java
+++ src/main/java/Module_2/util/TransactionUtil.java
@@ -1,17 +1,35 @@
 package Module_2.util;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-
+/**
+ * Утилитарный класс для управления транзакциями Hibernate.
+ * Предоставляет безопасное выполнение операций в транзакционном контексте.
+ */
 public class TransactionUtil {
     private static final Logger logger = LoggerFactory.getLogger(TransactionUtil.class);
 
+    /**
+     * Выполняет операцию с возвращаемым значением в транзакционном контексте.
+     *
+     * @param function Функция, принимающая сессию Hibernate и возвращающая результат типа T
+     * @param <T> Тип возвращаемого значения
+     * @return Результат выполнения функции
+     * @throws DatabaseException Если возникает ошибка при работе с базой данных
+     *
+     * @apiNote Автоматически управляет жизненным циклом транзакции:
+     *     1. Открывает сессию
+     *     2. Начинает транзакцию
+     *     3. Выполняет переданную функцию
+     *     4. Коммитит транзакцию при успехе
+     *     5. Откатывает транзакцию при ошибке
+     */
     public static <T> T doInTransaction(Function<Session, T> function) {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
@@ -32,6 +50,14 @@ public class TransactionUtil {
         }
     }
 
+    /**
+     * Выполняет операцию без возвращаемого значения в транзакционном контексте.
+     *
+     * @param consumer Операция, принимающая сессию Hibernate
+     *
+     * @see #doInTransaction(Function)
+     * @implNote Реализация преобразует Consumer в Function, возвращающую null
+     */
     public static void doInTransaction(Consumer<Session> consumer) {
         doInTransaction(session -> {
             consumer.accept(session);
