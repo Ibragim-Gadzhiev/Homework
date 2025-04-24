b7077d9acee367b59525a8078adb6e0fd5fd463d
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-04-24 11:06:27 +0300

    style(user): adjust formatting UserDao class

--- src/main/java/Module_2/dao/UserDao.java
+++ src/main/java/Module_2/dao/UserDao.java
@@ -7,9 +7,14 @@ import java.util.Optional;
 
 public interface UserDao {
     void create(User user);
+
     Optional<User> read(Long id);
+
     List<User> readAll();
+
     boolean update(User user);
+
     boolean delete(Long id);
+
     boolean existsByEmail(String email);
 }
