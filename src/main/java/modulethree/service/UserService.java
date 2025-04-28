eb0a226c745178901bd598dbab8dc964b75647a5
Ibragim Gadzhiev <unknown.nvme@gmail.com>
2025-04-28 16:53:15 +0300

    :sparkles: feat(service): add UserService class

--- src/main/java/modulethree/service/UserService.java
+++ src/main/java/modulethree/service/UserService.java
@@ -1,4 +1,34 @@
 package modulethree.service;
 
+import java.util.List;
+import java.util.Optional;
+import modulethree.dao.UserDao;
+import modulethree.model.User;
+
 public class UserService {
+    private final UserDao userDao;
+
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void createUser(User user) {
+        userDao.create(user);
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userDao.read(id);
+    }
+
+    public List<User> getAllUsers() {
+        return userDao.readAll();
+    }
+
+    public boolean updateUser(User user) {
+        return userDao.update(user);
+    }
+
+    public boolean deleteUser(Long id) {
+        return userDao.delete(id);
+    }
 }
